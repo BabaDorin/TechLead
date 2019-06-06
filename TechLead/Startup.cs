735a76acbcb9bc18d4e18679ed64d14cd7745250
Baba Dorin <bvd.dorin@gmail.com>
2019-06-06 16:38:05 +0300

    Add project files.

---
+++ TechLead/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechLead.Startup))]
+namespace TechLead
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
