23cfa642bc94c3fc4320697c5e8b99004cf78c36
Baba Dorin <bvd.dorin@gmail.com>
2019-06-06 16:45:53 +0300

    Creating database and core classes

---
+++ TechLead/Models/Domain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechLead.Models
+{
+    public class Domain
+    {
+        public int Id { get; set; }
+        private string Name { get; set; }
+    }
+}
---
+++ TechLead/Models/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechLead.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ApplicationUser Author { get; set; } 
+        public Domain Domain { get; set; }
+    }
+}
