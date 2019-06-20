f3e5392241e6cb8da51c15810d65b9c88a549293
BabaDorin <bvd.dorin@gmail.com>
2019-06-20 15:45:31 +0300

    Adding branches and links to them

---
+++ TechLead/Controllers/Branch2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechLead.Controllers
+{
+    public class Branch2Controller : Controller
+    {
+        // GET: Branch2
+        public ActionResult Secondbranch()
+        {
+            return View();
+        }
+    }
+}
---
+++ TechLead/Controllers/Branch3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechLead.Controllers
+{
+    public class Branch3Controller : Controller
+    {
+        // GET: Branch3
+        public ActionResult Thirdbranch()
+        {
+            return View();
+        }
+    }
+}
