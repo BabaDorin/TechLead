f1dd581b4bce768549bdf66b81a0adb8bc7269a7
BabaDorin <bvd.dorin@gmail.com>
2019-07-03 23:46:37 +0300

    Design the Create view of the Problem controller.

--- TechLead/Models/Exercise.cs
+++ TechLead/Models/Exercise.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,22 +9,45 @@ namespace TechLead.Models
     public class Exercise
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Range(10,400)]
         public int Points { get; set; }
+
         public int SubmissionsAbove10Points { get; set; }
+
         public int SubmissionsUnder10Points { get; set; }
-        public ApplicationUser Author { get; set; } 
+
+        public ApplicationUser Author { get; set; }
+
+        [Required]
         public IEnumerable<Difficulty> Difficulty { get; set; }
+
+        [Required]
         public string Condition { get; set; }
+
+        [Required]
         public string Imput1 { get; set; }
+
         public string Imput2 { get; set; }
+
         public string Imput3 { get; set; }
+
         public string Imput4 { get; set; }
+
         public string Imput5 { get; set; }
+
+        [Required]
         public string Output1 { get; set; }
+
         public string Output2 { get; set; }
+
         public string Output3 { get; set; }
+
         public string Output4 { get; set; }
+
         public string Output5 { get; set; }
 
         public int Submissions()
