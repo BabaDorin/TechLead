0ecc6cd0d4cbe043463ad9574bacb2cfec679553
BabaDorin <bvd.dorin@gmail.com>
2019-07-17 00:25:56 +0300

    Working on builing a register pop-up modal with _RegisterPartialView

--- TechLead/Controllers/AccountController.cs
+++ TechLead/Controllers/AccountController.cs
@@ -175,7 +175,7 @@ namespace TechLead.Controllers
             }
 
             // If we got this far, something failed, redisplay form
-            return View(model);
+            return View("_RegisterPartialView",model);
         }
 
         //
