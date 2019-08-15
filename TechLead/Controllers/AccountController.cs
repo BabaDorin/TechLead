9343d85d208451eeeaa2751e9994b79704e10720
BabaDorin <bvd.dorin@gmail.com>
2019-08-15 10:45:46 +0300

    Repairing "Log In via Email"

--- TechLead/Controllers/AccountController.cs
+++ TechLead/Controllers/AccountController.cs
@@ -78,9 +78,12 @@ namespace TechLead.Controllers
                 return View(model);
             }
 
+            var user = await UserManager.FindByEmailAsync(model.Email);
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = user == null ? SignInStatus.Failure : await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
