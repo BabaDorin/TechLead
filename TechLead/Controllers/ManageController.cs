f05d5b61f5049093e5619fb74bd765b77f628063
BabaDorin <bvd.dorin@gmail.com>
2019-08-24 22:40:13 +0300

    Developing "Index", "Manage" view

--- TechLead/Controllers/ManageController.cs
+++ TechLead/Controllers/ManageController.cs
@@ -76,28 +76,6 @@ namespace TechLead.Controllers
                 Logins = await UserManager.GetLoginsAsync(userId),
                 BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
             };
-
-            Exception ex = Server.GetLastError();
-
-            if (ex is HttpRequestValidationException)
-            {
-                Response.Clear();
-                Response.StatusCode = 200;
-                Response.Write(@"
-<html><head><title>HTML Not Allowed</title>
-<script language='JavaScript'><!--
-function back() { history.go(-1); } //--></script></head>
-<body style='font-family: Arial, Sans-serif;'>
-<h1>Oops!</h1>
-<p>I'm sorry, but HTML entry is not allowed on that page.</p>
-<p>Please make sure that your entries do not contain 
-any angle brackets like &lt; or &gt;.</p>
-<p><a href='javascript:back()'>Go back</a></p>
-</body></html>
-");
-                Response.End();
-            }
-
             return View(model);
         }
 
