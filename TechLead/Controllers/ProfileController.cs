ce6f84f49b0057ceb31f1c7b5f37f0fdc0a27204
BabaDorin <bvd.dorin@gmail.com>
2019-09-07 22:34:17 +0300

    Adding ViewProfile (Controller, Functionalities, View)

--- TechLead/Controllers/ProfileController.cs
+++ TechLead/Controllers/ProfileController.cs
@@ -12,7 +12,11 @@ namespace TechLead.Controllers
         ApplicationDbContext _context = new ApplicationDbContext();
         public ActionResult ViewProfile(string userID)
         {
-            return View();
+            ProfileViewModel model;
+            ApplicationUser User = _context.Users.Find(userID);
+            model = new ProfileViewModel(User.UserName, User.PhoneNumber, User.About,
+                User.TotalPoints, User.FirstRegistration, User.Job, User.Email);
+            return View(model);
         }
     }
 }
--- TechLead/Models/ProfileViewModels.cs
+++ TechLead/Models/ProfileViewModels.cs
@@ -12,5 +12,16 @@ namespace TechLead.Models
         public DateTime FirstRegistration { get; set; }
         public string Job { get; set; }
         public string Email { get; set; }
+        public ProfileViewModel(string UserName, string PhoneNumber, string About, double TotalPoints,
+            DateTime FirstRegistration, string Job, string Email)
+        {
+            this.UserName = UserName;
+            this.PhoneNumber = PhoneNumber;
+            this.About = About;
+            this.TotalPoints = TotalPoints;
+            this.FirstRegistration = FirstRegistration;
+            this.Job = Job;
+            this.Email = Email;
+        }
     }
 }
