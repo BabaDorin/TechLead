b7fba54d27500849d6add1bc7bf0d852032fe3cf
BabaDorin <bvd.dorin@gmail.com>
2019-09-08 15:22:14 +0300

    Deleting cropper plugin.

--- TechLead/Controllers/ManageController.cs
+++ TechLead/Controllers/ManageController.cs
@@ -382,7 +382,7 @@ namespace TechLead.Controllers
 
             base.Dispose(disposing);
         }
-
+        
 #region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
