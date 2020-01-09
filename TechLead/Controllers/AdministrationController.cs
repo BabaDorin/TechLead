61933afe264dceac27e231002101783ecce740d9
BabaDorin <bvd.dorin@gmail.com>
2020-01-09 22:44:31 +0200

    Implementing role sistem [ON HOLD]

--- TechLead/Controllers/AdministrationController.cs
+++ TechLead/Controllers/AdministrationController.cs
@@ -1,17 +1,49 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TechLead.Models;
 
 namespace TechLead.Controllers
 {
     public class AdministrationController : Controller
     {
-        // GET: Administration
-        public ActionResult Index()
+        private readonly RoleManager<IdentityRole> roleManager;
+        
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public ActionResult CreateRole()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateRole(CreateRoleViewModel createRole)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = createRole.RoleName
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "Home");
+                }
+            }
+
+            return View(createRole);
+
         }
     }
 }
