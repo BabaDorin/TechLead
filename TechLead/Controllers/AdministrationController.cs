814f2d15adaf58772f553ae134d12f0e596ed54a
BabaDorin <bvd.dorin@gmail.com>
2020-01-09 21:54:46 +0200

    Adding CreateRoleViewModel [] Adding Administration Controller

---
+++ TechLead/Controllers/AdministrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechLead.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: Administration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
