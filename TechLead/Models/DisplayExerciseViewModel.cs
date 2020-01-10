4203997a81252aa5e86fe7a630542f8bf539f937
BabaDorin <bvd.dorin@gmail.com>
2020-01-10 16:02:42 +0200

    Adding DisplayExerciseGeneralInfoViewModel

---
+++ TechLead/Models/DisplayExerciseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechLead.Models
+{
+    public class DisplayExerciseGeneralInfoViewModel
+    {
+        public int Id;
+        public string Name;
+        public int Points;
+        public int DifficultyID;
+        public string Author;
+        public string AuthorID;
+    }
+}
