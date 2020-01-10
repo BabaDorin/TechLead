e7b2781db7cbce76b19d149062169a49a015caae
BabaDorin <bvd.dorin@gmail.com>
2020-01-10 13:18:52 +0200

    Adding Create Roles Method in Startup.cs

--- TechLead/Startup.cs
+++ TechLead/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 using TechLead.Models;
 
@@ -10,11 +12,26 @@ namespace TechLead
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRoles();
         }
 
         private void CreateRoles()
         {
             ApplicationDbContext _context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+            }
 
         }
     }
