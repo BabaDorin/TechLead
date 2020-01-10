79194304cf5e08066a2a4add0c320858321a040f
BabaDorin <bvd.dorin@gmail.com>
2020-01-10 13:24:24 +0200

    Adding UserRole property to ApplicationUser Model

---
+++ TechLead/Migrations/202001101123470_Add_UserRole_PropertyToApplicationUser.cs
@@ -0,0 +1,18 @@
+namespace TechLead.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_UserRole_PropertyToApplicationUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "UserRole", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "UserRole");
+        }
+    }
+}
